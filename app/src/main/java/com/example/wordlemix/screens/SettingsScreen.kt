2122f167a938fe407393cce88ca4df23964dac6e
annapoglitsch <anna.poglitsch@outlook.de>
2024-06-24 12:35:31 +0200

    Dark- and Lightmode implemented

--- app/src/main/java/com/example/wordlemix/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/wordlemix/screens/SettingsScreen.kt
@@ -1,3 +1,5 @@
+@file:Suppress("IMPLICIT_CAST_TO_ANY")
+
 package com.example.wordlemix.screens
 
 import android.annotation.SuppressLint
@@ -20,15 +22,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Typography
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -44,107 +49,151 @@ import com.example.wordlemix.reusableItems.AppBars
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun SettingsScreen(navController: NavController, route: String, darkTheme: Boolean, onThemeUpdated: () -> Unit) {
+fun SettingsScreen(
+    navController: NavController,
+    route: String,
+    darkTheme: Boolean,
+    onThemeUpdated: () -> Unit
+) {
 
     val topAppBar = AppBars()
-    Surface(
-        modifier = Modifier
-            .fillMaxSize(),
-    ) {
-
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            topBar = {
-                topAppBar.TopAppBar(titleText = "Settings", icon = true, navController = navController)
-            }
+    var isDarkTheme by remember { mutableStateOf(false) }
+    ThemeSwitch(darkTheme = isDarkTheme) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize(),
+            color = Color(0xFFAAD6F3)
         ) {
-            Column(
-                Modifier
-                    .fillMaxSize()
-                    .background(color = Color(0xFFAAD6F3))
-                    .padding(top = 16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    topAppBar.TopAppBar(
+                        titleText = "Settings",
+                        icon = true,
+                        navController = navController
+                    )
+                }
             ) {
-                ThemeSwitcher(darkTheme = darkTheme,
-                    onClick = onThemeUpdated)
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(top = 16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    /*  ThemeSwitcherToggle(
+                          darkTheme = darkTheme,
+                          onClick = onThemeUpdated
+                      )*/
+                    ThemeSwitchToggle(
+                        darkTheme = isDarkTheme,
+                        onClick = { isDarkTheme = !isDarkTheme })
+                }
+            }
+        }
+
+    }
+
+}
+@Composable
+fun ThemeSwitch(darkTheme: Boolean = false, content: @Composable () -> Unit) {
+    val color = if (darkTheme) {
+        darkColorScheme(
+            primary = Color(0xFFAAD6F3),
+            primaryContainer = Color.Black,
+            secondary = Color.Gray,
+        )
+    } else {
+        lightColorScheme(
+            primary = Color.Black,
+            primaryContainer = Color(0xFFAAD6F3),
+            secondary = Color.Gray
+
+            )
+    }
+    MaterialTheme(
+        colorScheme = color,
+        typography = Typography(),
+        content = content
+    )
+}
+
+
+//ToDo: default background should be Color(0xFFAAD6F3) (lightmode)
+
+//followed this tutorial: https://www.youtube.com/watch?v=Nvphdmi-6qc
+@Composable
+fun ThemeSwitchToggle(
+    darkTheme: Boolean = false,
+    size: Dp = 100.dp,
+    iconSize: Dp = size / 3,
+    padding: Dp = 10.dp,
+    borderWidth: Dp = 1.dp,
+    parentShape: RoundedCornerShape = CircleShape,
+    toggleShape: RoundedCornerShape = CircleShape,
+    animationSpec: AnimationSpec<Dp> = tween(durationMillis = 300),
+    onClick: () -> Unit
+) {
+    val offset by animateDpAsState(
+        targetValue = if (darkTheme) 0.dp else size,
+        animationSpec = animationSpec
+    )
+
+    Box(modifier = Modifier
+        .width(size * 2)
+        .height(size)
+        .clip(shape = parentShape)
+        .clickable {
+            onClick()
+        }
+        .background(MaterialTheme.colorScheme.secondaryContainer)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(size)
+                .offset(x = offset)
+                .padding(all = padding)
+                .clip(shape = toggleShape)
+                .background(MaterialTheme.colorScheme.primary)
+        ) {}
+        Row(
+            modifier = Modifier
+                .border(
+                    border = BorderStroke(
+                        width = borderWidth,
+                        color = MaterialTheme.colorScheme.primary
+                    ),
+                    shape = parentShape
+                )
+        ) {
+            Box(
+                modifier = Modifier.size(size),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    modifier = Modifier.size(iconSize),
+                    imageVector = ImageVector.vectorResource(id = R.drawable.baseline_dark_mode_24),
+                    contentDescription = "Dark Mode",
+                    tint = if (darkTheme) MaterialTheme.colorScheme.secondaryContainer
+                    else MaterialTheme.colorScheme.primary
+                )
+            }
+            Box(
+                modifier = Modifier.size(size),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    modifier = Modifier.size(iconSize),
+                    imageVector = ImageVector.vectorResource(id = R.drawable.baseline_light_mode_24),
+                    contentDescription = "Light Mode",
+                    tint = if (darkTheme) MaterialTheme.colorScheme.primary
+                    else MaterialTheme.colorScheme.secondaryContainer
+                )
             }
         }
     }
 }
-
-//ToDo: add logic to button -> themes must change
-
-    //followed this tutorial: https://www.youtube.com/watch?v=Nvphdmi-6qc
-    @Composable
-    fun ThemeSwitcher(
-        darkTheme: Boolean = false,
-        size: Dp = 100.dp,
-        iconSize: Dp = size / 3,
-        padding: Dp = 10.dp,
-        borderWidth: Dp = 1.dp,
-        parentShape: RoundedCornerShape = CircleShape,
-        toggleShape: RoundedCornerShape = CircleShape,
-        animationSpec: AnimationSpec<Dp> = tween(durationMillis = 300),
-        onClick: () -> Unit
-    ) {
-        val offset by animateDpAsState(
-            targetValue = if (darkTheme) 0.dp else size,
-            animationSpec = animationSpec
-        )
-
-        Box(modifier = Modifier
-            .width(size * 2)
-            .height(size)
-            .clip(shape = parentShape)
-            .clickable { onClick() }
-            .background(MaterialTheme.colorScheme.secondaryContainer)
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(size)
-                    .offset(x = offset)
-                    .padding(all = padding)
-                    .clip(shape = toggleShape)
-                    .background(MaterialTheme.colorScheme.primary)
-            ) {}
-            Row(
-                modifier = Modifier
-                    .border(
-                        border = BorderStroke(
-                            width = borderWidth,
-                            color = MaterialTheme.colorScheme.primary
-                        ),
-                        shape = parentShape
-                    )
-            ) {
-                Box(
-                    modifier = Modifier.size(size),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Icon(
-                        modifier = Modifier.size(iconSize),
-                        imageVector = ImageVector.vectorResource(id = R.drawable.baseline_dark_mode_24),
-                        contentDescription = "Dark Mode",
-                        tint = if (darkTheme) MaterialTheme.colorScheme.secondaryContainer
-                        else MaterialTheme.colorScheme.primary
-                    )
-                }
-                Box(
-                    modifier = Modifier.size(size),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Icon(
-                        modifier = Modifier.size(iconSize),
-                        imageVector = ImageVector.vectorResource(id = R.drawable.baseline_light_mode_24),
-                        contentDescription = "Light Mode",
-                        tint = if (darkTheme) MaterialTheme.colorScheme.primary
-                        else MaterialTheme.colorScheme.secondaryContainer
-                    )
-                }
-            }
-        }
-    }
 
 
 
