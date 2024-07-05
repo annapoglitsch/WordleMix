6521472747bf01ff97b27d9a5d3485804398d4b4
Anna Poglitsch <114026776+annapoglitsch@users.noreply.github.com>
2024-07-06 00:19:27 +0200

    WinningPanel

--- app/src/main/java/com/example/wordlemix/screens/GameScreen.kt
+++ app/src/main/java/com/example/wordlemix/screens/GameScreen.kt
@@ -3,10 +3,12 @@ package com.example.wordlemix.screens
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -33,6 +35,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -142,4 +145,18 @@ fun textfieldTempl(isEnabled: Boolean, fontColors: SnapshotStateList<Color>, bac
             )
         }
    }
+}
+
+@Preview
+@Composable
+fun winningPanel(scoreIncrease: Int = 2){
+    Column(modifier = Modifier
+        .background(color = Color.Gray.copy(alpha = 0.5f))
+        .width(200.dp)
+        .height(200.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "You won!")
+        Text(text = "Your score will be added by ${scoreIncrease}.")
+    }
 }
